170b15b0e31b2c31de6e3b4e76b34cec4a8e1b81
SimamkeleSefalane <102727091+SimamkeleSefalane@users.noreply.github.com>
2022-06-02 15:46:22 +0200

    Added a class and main page code

--- WeatherAppSln/WeatherApp/WeatherApp/MainPage.xaml.cs
+++ WeatherAppSln/WeatherApp/WeatherApp/MainPage.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace WeatherApp
@@ -14,5 +17,24 @@ namespace WeatherApp
         {
             InitializeComponent();
         }
-    }
+
+
+        private async void GetWeatherDetails()
+        {
+
+            var location = await Geolocation.GetLocationAsync();
+
+
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Add("Accept", "application /json");
+
+            var response = await client.GetStringAsync("https://api.openweathermap.org/data/2.5/weather?lat=lat&lon=lon&appid=0a174e1be268d8c199778d5f16059615");
+
+
+            var weatherDetails = JsonConvert.DeserializeObject<WeatherDetails>(response);
+
+        }
+    } 
+
 }
