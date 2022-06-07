db01076e79e6682485cbf06547409ee42ecbdf4f
SimamkeleSefalane <102727091+SimamkeleSefalane@users.noreply.github.com>
2022-06-07 16:02:15 +0200

    added more code

--- WeatherAppSln/WeatherApp/WeatherApp/MainPage.xaml.cs
+++ WeatherAppSln/WeatherApp/WeatherApp/MainPage.xaml.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.Forms;
+using static WeatherApp.WeatherDetails;
 
 namespace WeatherApp
 {
@@ -19,10 +20,22 @@ namespace WeatherApp
         }
 
 
-        private async void GetWeatherDetails()
-        {
+        private async Task<OpenWeatherDetails>GetWeatherDetails()
+        { 
+
+
+            var data = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+            if (data != PermissionStatus.Granted)
+            {
+                var newdata = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            }
+
 
             var location = await Geolocation.GetLocationAsync();
+
+            var latitude= location.Latitude;
+            var longitude= location.Longitude;
+
 
 
             var client = new HttpClient();
@@ -32,7 +45,9 @@ namespace WeatherApp
             var response = await client.GetStringAsync("https://api.openweathermap.org/data/2.5/weather?lat=lat&lon=lon&appid=0a174e1be268d8c199778d5f16059615");
 
 
-            var weatherDetails = JsonConvert.DeserializeObject<WeatherDetails>(response);
+            var weatherDetails = JsonConvert.DeserializeObject<OpenWeatherDetails>(response);
+
+            return weatherDetails;
 
         }
     } 
